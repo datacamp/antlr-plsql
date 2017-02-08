4fdfa819a18f9b0406b599d2fcffea3ed09b1ae4
Michael Chow <machow@princeton.edu>
2017-02-08 18:33:59 +0100

    fix selected_option in unit tests

--- sqlwhat/tests/test_test_mc.py
+++ sqlwhat/tests/test_test_mc.py
@@ -14,19 +14,19 @@ def prepare_state(student_code):
         student_conn = None, solution_conn = None)
 
 def test_mc_alone_pass():
-    state = prepare_state("solution_option = 1")
+    state = prepare_state("selected_option = 1")
     _test_mc(1, ['good', 'bad'], state=state)
 
 def test_mc_alone_fail():
-    state = prepare_state("solution_option = 2")
+    state = prepare_state("selected_option = 2")
     with pytest.raises(TF):
         _test_mc(1, ['good', 'bad'], state=state)
 
 def test_mc_chain_pass():
-    state = prepare_state("solution_option = 1")
+    state = prepare_state("selected_option = 1")
     Ex(state).test_mc(1, ['good', 'bad'])
 
 def test_mc_chain_fail():
-    state = prepare_state("solution_option = 2")
+    state = prepare_state("selected_option = 2")
     with pytest.raises(TF):
         Ex(state).test_mc(1, ['good', 'bad'])
