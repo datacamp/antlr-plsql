d638c4e1f473d5359dc04a8cc0745c686da4716c
Michael Chow <machow@princeton.edu>
2017-02-21 16:14:37 -0500

    bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 
 setup(
 	name='sqlwhat',
-	version='0.0.5',
+	version='0.0.6',
 	packages=['sqlwhat', 'sqlwhat.grammar.plsql'],
 	install_requires=['pythonwhat', 'antlr4-python3-runtime'])
