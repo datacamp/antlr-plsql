bbf289f2d6f1f6bed9ecd731805dcefaaab27465
Michael Chow <machow@princeton.edu>
2017-03-07 11:30:18 -0500

    allow versioning in __init__, setup travis

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 
+import re
+import ast
 from setuptools import setup
 
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('antlr_plsql/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
 setup(
-	name='antlr-plsql',
-	version='0.0.1',
-	packages=['antlr_plsql'],
-	install_requires=['antlr4-python3-runtime'])
+	name = 'antlr-plsql',
+	version = version,
+	packages = ['antlr_plsql'],
+	install_requires = ['antlr4-python3-runtime'],
+        description = 'A procedural sql parser, written in Antlr4',
+        author = 'Michael Chow',
+        author_email = 'michael@datacamp.com',
+        url = 'https://github.com/datacamp/antlr-plsql')
