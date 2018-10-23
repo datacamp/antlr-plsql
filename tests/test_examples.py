b2b44dfcc488840a116cf1857cba5c45fa1201a9
Jeroen Hermans <jeroen@datacamp.com>
2018-10-23 14:59:29 +0200

    Execute more tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,21 +3,48 @@ import os
 from antlr_plsql import ast
 
 crnt_dir = os.path.dirname(__file__)
-examples = os.path.join(crnt_dir, 'examples')
-#examples_sql_script = os.path.join(crnt_dir, 'examples-sql-script')
+examples = os.path.join(crnt_dir, "examples")
+# examples_sql_script = os.path.join(crnt_dir, "examples-sql-script")
+
 
 def load_examples(dir_path):
-    return [[fname, open(os.path.join(dir_path, fname)).read()] 
-                for fname in os.listdir(dir_path)
-                ]
+    return [
+        [fname, open(os.path.join(dir_path, fname)).read()]
+        for fname in os.listdir(dir_path)
+    ]
 
-@pytest.mark.examples
-@pytest.mark.parametrize('name, query', load_examples(examples))
+
+# @pytest.mark.examples
+@pytest.mark.parametrize("name, query", load_examples(examples))
 def test_examples(name, query):
     ast.parse(query)
 
-#@pytest.mark.parametrize('name, query', load_examples(examples_sql_script))
-#def test_examples_sql_script(name, query):
-#    ast.parse(query)
+
+# @pytest.mark.parametrize("name, query", load_examples(examples_sql_script))
+# def test_examples_sql_script(name, query):
+#     ast.parse(query)
 
 
+def load_dump(fname):
+    import yaml
+
+    dirname = os.path.dirname(__file__)
+    dump_data = yaml.load(open(dirname + "/" + fname))
+
+    all_cmds = []
+    for start, cmds in dump_data.items():
+        for cmd, res in cmds:
+            all_cmds.append((start, cmd, res))
+    return all_cmds
+
+
+@pytest.mark.parametrize(
+    "start,cmd,res",
+    [
+        *load_dump("dump_v0.2.yml"),
+        *load_dump("dump_v0.3.yml"),
+        *load_dump("dump_v0.5.yml"),
+    ],
+)
+def test_dump(start, cmd, res):
+    assert repr(ast.parse(cmd, start, strict=True)) == res
