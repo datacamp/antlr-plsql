1a02f1d5430041e6c916bc9f8ca8d6a9df6f0850
Jeroen Hermans <jeroen@datacamp.com>
2018-12-20 11:14:13 +0100

    Format ast code

--- antlr_plsql/ast.py
+++ antlr_plsql/ast.py
@@ -10,11 +10,14 @@ from . import plsql_grammar
 #       sql_script
 
 
-def parse(sql_text, start='sql_script', strict=False):
-    return antlr_ast.parse(plsql_grammar, AstVisitor(), sql_text, start, strict)
+def parse(sql_text, start="sql_script", strict=False):
+    tree = antlr_ast.parse(plsql_grammar, sql_text, start, strict)
+    return AstVisitor().visit(tree)
 
 
 import yaml
+
+
 def parse_from_yaml(fname):
     data = yaml.load(open(fname)) if isinstance(fname, str) else fname
     out = {}
@@ -24,7 +27,7 @@ def parse_from_yaml(fname):
 
 
 class Unshaped(AstNode):
-    _fields = ['arr']
+    _fields = ["arr"]
 
     def __init__(self, ctx, arr=tuple()):
         self.arr = arr
@@ -32,7 +35,7 @@ class Unshaped(AstNode):
 
 
 class Script(AstNode):
-    _fields = ['body']
+    _fields = ["body"]
     _priority = 0
 
     @classmethod
@@ -43,13 +46,24 @@ class Script(AstNode):
             if not isinstance(child, Tree.TerminalNodeImpl):
                 body.append(visitor.visit(child))
 
-        return cls(ctx, body = body)
+        return cls(ctx, body=body)
 
 
 class SelectStmt(AstNode):
-    _fields = ['pref', 'target_list', 'into_clause', 'from_clause', 'where_clause',
-               'hierarchical_query_clause', 'group_by_clause', 'having_clause', 'model_clause', 
-               'for_update_clause', 'order_by_clause', 'limit_clause']
+    _fields = [
+        "pref",
+        "target_list",
+        "into_clause",
+        "from_clause",
+        "where_clause",
+        "hierarchical_query_clause",
+        "group_by_clause",
+        "having_clause",
+        "model_clause",
+        "for_update_clause",
+        "order_by_clause",
+        "limit_clause",
+    ]
 
     _priority = 1
 
@@ -58,7 +72,7 @@ class SelectStmt(AstNode):
         # allowing arbitrary order for some clauses, means their results are lists w/single item
         # could also do testing to make sure clauses weren't specified multiple times
         query = cls._from_fields(visitor, ctx)
-        unlist_clauses = cls._fields[cls._fields.index('group_by_clause'): ]
+        unlist_clauses = cls._fields[cls._fields.index("group_by_clause") :]
         for k in unlist_clauses:
             attr = getattr(query, k, [])
             if isinstance(attr, list):
@@ -68,7 +82,7 @@ class SelectStmt(AstNode):
 
 
 class Union(AstNode):
-    _fields = ['left', 'op', 'right', 'order_by_clause']        
+    _fields = ["left", "op", "right", "order_by_clause"]
 
     @classmethod
     def _from_subquery_compound(cls, visitor, ctx):
@@ -76,16 +90,17 @@ class Union(AstNode):
         # since the final ORDER BY applies to the entire statement (not just subquery)
         union = cls._from_fields(visitor, ctx)
         if not isinstance(ctx.right, plsql_grammar.Parser.SubqueryParenContext):
-            order_by = getattr(union.right, 'order_by_clause', None)
+            order_by = getattr(union.right, "order_by_clause", None)
             union.order_by_clause = order_by
             # remove from right SELECT
-            if order_by: union.right.order_by_clause = None
+            if order_by:
+                union.right.order_by_clause = None
 
         return union
 
 
 class Identifier(AstNode):
-    _fields = ['fields']
+    _fields = ["fields"]
 
 
 # TODO
@@ -94,13 +109,21 @@ class Star(AstNode):
 
 
 class AliasExpr(AstNode):
-    _fields = ['expr', 'alias']
+    _fields = ["expr", "alias"]
 
 
 class BinaryExpr(AstNode):
-    _fields = ['left', 'op', 'right']
-    _rules = ['IsExpr', 'InExpr', 'BetweenExpr', 'LikeExpr', 'RelExpr', 
-              'MemberExpr', 'AndExpr', 'OrExpr']
+    _fields = ["left", "op", "right"]
+    _rules = [
+        "IsExpr",
+        "InExpr",
+        "BetweenExpr",
+        "LikeExpr",
+        "RelExpr",
+        "MemberExpr",
+        "AndExpr",
+        "OrExpr",
+    ]
 
     @classmethod
     def _from_mod(cls, visitor, ctx):
@@ -116,7 +139,7 @@ class BinaryExpr(AstNode):
         # NOT IN produces unary expression
         bin_or_unary = cls._from_mod(visitor, ctx)
         right = visitor.visit(ctx.subquery() or ctx.expression_list())
-        if isinstance(bin_or_unary, UnaryExpr): 
+        if isinstance(bin_or_unary, UnaryExpr):
             bin_or_unary.expr.right = right
         else:
             bin_or_unary.right = right
@@ -124,24 +147,29 @@ class BinaryExpr(AstNode):
 
 
 class UnaryExpr(AstNode):
-    _fields = ['op', 'unary_expression->expr']
+    _fields = ["op", "unary_expression->expr"]
 
 
 class OrderByExpr(AstNode):
-    _fields = ['order_by_elements->expr']
-    #_rules = ['order_by_clause']
+    _fields = ["order_by_elements->expr"]
+    # _rules = ['order_by_clause']
 
 
 class SortBy(AstNode):
-    _fields = ['expression->expr', 'direction', 'nulls']
-    #_rules = ['order_by_elements']
+    _fields = ["expression->expr", "direction", "nulls"]
+    # _rules = ['order_by_elements']
 
 
 class JoinExpr(AstNode):
-    _fields = ['left', 'join_type', 'table_ref->right',
-               'join_on_part->cond', 
-               # fields below are Oracle specific
-               'join_using_part->using', 'query_partition_clause']
+    _fields = [
+        "left",
+        "join_type",
+        "table_ref->right",
+        "join_on_part->cond",
+        # fields below are Oracle specific
+        "join_using_part->using",
+        "query_partition_clause",
+    ]
 
     @classmethod
     def _from_table_ref(cls, visitor, ctx):
@@ -153,23 +181,36 @@ class JoinExpr(AstNode):
 
 
 from collections.abc import Sequence
+
+
 class Call(AstNode):
-    _fields = ['name', 'pref', 'args', 'function_argument_analytic->args', 'concatenation->args', 'over_clause']
+    _fields = [
+        "name",
+        "pref",
+        "args",
+        "function_argument_analytic->args",
+        "concatenation->args",
+        "over_clause",
+    ]
 
     @staticmethod
-    def get_name(ctx): return ctx.children[0].getText().upper()
+    def get_name(ctx):
+        return ctx.children[0].getText().upper()
 
     @classmethod
     def _from_aggregate_call(cls, visitor, ctx):
         obj = cls._from_fields(visitor, ctx)
         obj.name = cls.get_name(ctx)
-        
-        if obj.args is None: obj.args = []
-        elif not isinstance(obj.args, Sequence): obj.args = [obj.args]
+
+        if obj.args is None:
+            obj.args = []
+        elif not isinstance(obj.args, Sequence):
+            obj.args = [obj.args]
         return obj
 
 
 # PARSE TREE VISITOR ----------------------------------------------------------
+
 
 class AstVisitor(plsql_grammar.Visitor):
     def visitChildren(self, node, predicate=None):
@@ -180,7 +221,8 @@ class AstVisitor(plsql_grammar.Visitor):
                 return
 
             c = node.getChild(i)
-            if predicate and not predicate(c): continue
+            if predicate and not predicate(c):
+                continue
 
             childResult = c.accept(self)
             result = self.aggregateResult(result, childResult)
@@ -189,11 +231,18 @@ class AstVisitor(plsql_grammar.Visitor):
 
     @staticmethod
     def result_to_ast(node, result):
-        if len(result) == 1: return result[0]
-        elif len(result) == 0: return None
-        elif all(isinstance(res, str) for res in result): return " ".join(result)
-        elif all(isinstance(res, AstNode) and not isinstance(res, Unshaped) for res in result): return result
-        else: return Unshaped(node, result)
+        if len(result) == 1:
+            return result[0]
+        elif len(result) == 0:
+            return None
+        elif all(isinstance(res, str) for res in result):
+            return " ".join(result)
+        elif all(
+            isinstance(res, AstNode) and not isinstance(res, Unshaped) for res in result
+        ):
+            return result
+        else:
+            return Unshaped(node, result)
 
     def defaultResult(self):
         return list()
@@ -205,7 +254,7 @@ class AstVisitor(plsql_grammar.Visitor):
     def visitTerminal(self, ctx):
         """converting case insensitive keywords and identifiers to lowercase"""
         text = ctx.getText()
-        quotes = ['\'', '"']
+        quotes = ["'", '"']
         if not (text[0] in quotes and text[-1] in quotes):
             text = text.lower()
         return text
@@ -238,7 +287,7 @@ class AstVisitor(plsql_grammar.Visitor):
             return AliasExpr._from_fields(self, ctx)
         else:
             return self.visitChildren(ctx)
-    
+
     def visitOrder_by_clause(self, ctx):
         return OrderByExpr._from_fields(self, ctx)
 
@@ -267,7 +316,7 @@ class AstVisitor(plsql_grammar.Visitor):
         else:
             return self.visitChildren(ctx)
 
-    #def visitIs_part(self, ctx):
+    # def visitIs_part(self, ctx):
     #    return ctx
 
     # many outer label visitors ----------------------------------------------
@@ -277,53 +326,62 @@ class AstVisitor(plsql_grammar.Visitor):
     def visitInExpr(self, ctx):
         return BinaryExpr._from_in_expr(self, ctx)
 
-    visitIsExpr =       visitBinaryExpr
+    visitIsExpr = visitBinaryExpr
     visitBetweenExpr = visitModExpr
-    visitLikeExpr =    visitModExpr
-    visitRelExpr =     visitBinaryExpr
-    visitMemberExpr =  visitBinaryExpr
-    visitCursorExpr =  visitUnaryExpr
-    visitNotExpr =     visitUnaryExpr
-    visitAndExpr =     visitBinaryExpr
-    visitOrExpr =      visitBinaryExpr
-
+    visitLikeExpr = visitModExpr
+    visitRelExpr = visitBinaryExpr
+    visitMemberExpr = visitBinaryExpr
+    visitCursorExpr = visitUnaryExpr
+    visitNotExpr = visitUnaryExpr
+    visitAndExpr = visitBinaryExpr
+    visitOrExpr = visitBinaryExpr
 
     # simple dropping of tokens -----------------------------------------------
     # Note can't filter out TerminalNodeImpl from some currently as in something like
     # "SELECT a FROM b WHERE 1", the 1 will be a terminal node in where_clause
     def visitWhere_clause(self, ctx):
-        return self.visitChildren(ctx, predicate = lambda n: n is not ctx.WHERE())
+        return self.visitChildren(ctx, predicate=lambda n: n is not ctx.WHERE())
 
     def visitFrom_clause(self, ctx):
-        return  self.visitChildren(ctx, predicate = lambda n: not isinstance(n, Tree.TerminalNode))
+        return self.visitChildren(
+            ctx, predicate=lambda n: not isinstance(n, Tree.TerminalNode)
+        )
 
     def visitLimit_clause(self, ctx):
-        return  self.visitChildren(ctx, predicate = lambda n: n is not ctx.LIMIT())
+        return self.visitChildren(ctx, predicate=lambda n: n is not ctx.LIMIT())
 
     def visitColumn_alias(self, ctx):
-        return self.visitChildren(ctx, predicate = lambda n: n is not ctx.AS())
+        return self.visitChildren(ctx, predicate=lambda n: n is not ctx.AS())
 
     def visitTable_ref_list(self, ctx):
-        return self.visitChildren(ctx, predicate = lambda n: not isinstance(n, Tree.TerminalNodeImpl))
+        return self.visitChildren(
+            ctx, predicate=lambda n: not isinstance(n, Tree.TerminalNodeImpl)
+        )
 
     def visitGroup_by_clause(self, ctx):
-        return self.visitChildren(ctx, predicate = lambda n: not isinstance(n, Tree.TerminalNodeImpl))
+        return self.visitChildren(
+            ctx, predicate=lambda n: not isinstance(n, Tree.TerminalNodeImpl)
+        )
 
     def visitHaving_clause(self, ctx):
-        return  self.visitChildren(ctx, predicate = lambda n: n is not ctx.HAVING())
+        return self.visitChildren(ctx, predicate=lambda n: n is not ctx.HAVING())
 
     def visitExpression_list(self, ctx):
         return [self.visit(expr) for expr in ctx.expression()]
-        #return self.visitChildren(ctx, predicate = lambda n: not isinstance(n, Tree.TerminalNode))
+        # return self.visitChildren(ctx, predicate = lambda n: not isinstance(n, Tree.TerminalNode))
 
     def visitInto_clause(self, ctx):
         return [self.visit(expr) for expr in ctx.variable_name()]
 
     def visitJoin_on_part(self, ctx):
-        return self.visitChildren(ctx, predicate = lambda n: not isinstance(n, Tree.TerminalNode))
+        return self.visitChildren(
+            ctx, predicate=lambda n: not isinstance(n, Tree.TerminalNode)
+        )
 
     def visitJoin_using_part(self, ctx):
-        return self.visitChildren(ctx, predicate = lambda n: not isinstance(n, Tree.TerminalNode))
+        return self.visitChildren(
+            ctx, predicate=lambda n: not isinstance(n, Tree.TerminalNode)
+        )
 
     # removing parentheses ----------------------------------------------------
 
@@ -331,22 +389,25 @@ class AstVisitor(plsql_grammar.Visitor):
     def _exclude_parens(node):
         return not isinstance(node, Tree.TerminalNodeImpl)
 
-    def visitAtom(self, ctx): 
-        return self.visitChildren(ctx, predicate = self._exclude_parens)
+    def visitAtom(self, ctx):
+        return self.visitChildren(ctx, predicate=self._exclude_parens)
 
     visitParenExpr = visitAtom
     visitParenBinaryExpr = visitAtom
 
+
 # TODO: for some reason can't get this to work as in tsql
-#import inspect
-#for item in list(globals().values()):
-#    if inspect.isclass(item) and issubclass(item, AstNode):
-#        item._bind_to_visitor(AstVisitor)
+# import inspect
+# for item in list(globals().values()):
+#     if inspect.isclass(item) and issubclass(item, AstNode):
+#         item._bind_to_visitor(AstVisitor)
 
 
 import pkg_resources
-speaker_cfg = yaml.load(pkg_resources.resource_stream('antlr_plsql', 'speaker.yml'))
+
+speaker_cfg = yaml.load(pkg_resources.resource_stream("antlr_plsql", "speaker.yml"))
 speaker = Speaker(**speaker_cfg)
 
 if __name__ == "__main__":
-    parse('SELECT id FROM artists WHERE id > 100')
+    parse("SELECT id FROM artists WHERE id > 100")
+
