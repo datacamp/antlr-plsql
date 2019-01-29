a76f7bd115b808d97a73a13a2fad5f746524a120
Jeroen Hermans <jeroen@datacamp.com>
2019-01-29 22:59:00 +0100

    Update grammar

--- antlr_plsql/__init__.py
+++ antlr_plsql/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 from . import antlr_py as grammar
 from . import ast
--- antlr_plsql/ast.py
+++ antlr_plsql/ast.py
@@ -5,7 +5,7 @@ from antlr_ast.ast import (
     bind_to_visitor,
     AstNode,
     Speaker,
-    AntlrException as ParseError,
+    AntlrException as ParseError,  # noinspection PyUnresolvedReferences
 )
 
 from . import grammar
@@ -273,6 +273,7 @@ class Call(AstNode):
 
     @classmethod
     def _from_todo(cls, visitor, ctx):
+        # todo: refactor (grammar shouldn't have TodoCall)
         obj = cls._from_fields(visitor, ctx)
         regular_id_ctx = ctx.regular_id()
         if regular_id_ctx is not None:
@@ -287,6 +288,17 @@ class Call(AstNode):
                 if over_clause_ctx is not None:
                     obj.over_clause = visitor.visit(over_clause_ctx)
         return obj
+
+
+class Cast(AstNode):
+    _rules = ["CastCall"]
+    _fields_spec = [
+        "type_spec->type",
+        "subquery->statement",
+        "concatenation->statement",
+        "expression->statement",
+        "atom->statement",
+    ]
 
 
 class OverClause(AstNode):
@@ -313,6 +325,199 @@ class CaseWhen(AstNode):
     _rules = ["simple_case_when_part", "searched_case_when_part"]
 
 
+class PartitionBy(AstNode):
+    _rules = ["query_partition_clause"]
+    _fields_spec = ["expression"]
+
+
+class RenameColumn(AstNode):
+    _rules = ["rename_column_clause"]
+    _fields_spec = ["old_column_name->old_name", "new_column_name->new_name"]
+
+
+class Column(AstNode):
+    _rules = ["column_definition"]
+    _fields_spec = [
+        "column_name->name",
+        "datatype->data_type",
+        "type_name->data_type",
+        "inline_constraint->constraints",
+    ]
+
+
+class AddColumns(AstNode):
+    _rules = ["add_column_clause"]
+    _fields_spec = ["column_definition->columns"]
+
+
+class DropColumn(AstNode):
+    _rules = ["drop_column_clause"]
+    _fields_spec = ["names"]
+
+
+class AlterColumn(AstNode):
+    _rules = ["alter_column_clause"]
+    _fields_spec = ["column_name->name", "op", "datatype->data_type", "expression"]
+
+
+class DropConstraint(AstNode):
+    _rules = ["drop_constraint_clause"]
+    _fields_spec = ["drop_primary_key_or_unique_or_generic_clause->name"]
+
+
+class Reference(AstNode):
+    _rules = [("references_clause", "_from_references")]
+    _fields_spec = ["tableview_name->table", "columns"]
+
+    @classmethod
+    def _from_references(cls, visitor, ctx):
+        obj = cls._from_fields(visitor, ctx)
+        obj.columns = visitor.visitChildren(
+            ctx.paren_column_list().column_list(), predicate=is_terminal, simplify=False
+        )
+        return obj
+
+
+class CreateTable(AstNode):
+    _rules = [("create_table", "_from_table")]
+    _fields_spec = [
+        "tableview_name->name",
+        "TEMPORARY->temporary",
+        "select_statement->query",
+        "columns",
+    ]
+
+    @classmethod
+    def _from_table(cls, visitor, ctx):
+        obj = cls._from_fields(visitor, ctx)
+
+        relational_properties_ctx = ctx.relational_table().relational_properties()
+        if relational_properties_ctx:
+            obj.columns = visitor.visitChildren(
+                relational_properties_ctx, predicate=is_terminal, simplify=False
+            )
+        return obj
+
+
+class AlterTable(AstNode):
+    _rules = ["alter_table"]
+    _fields_spec = [
+        "tableview_name->name",
+        "column_clauses->changes",
+        "constraint_clauses->changes",
+    ]
+
+
+class AddConstraints(AstNode):
+    _fields_spec = ["out_of_line_constraint->constraints"]
+
+
+class DropTable(AstNode):
+    _rules = [("drop_table", "_from_table")]
+    _fields_spec = ["tableview_name->name", "existence_check"]
+
+    @classmethod
+    def _from_table(cls, visitor, ctx):
+        obj = cls._from_fields(visitor, ctx)
+
+        if ctx.IF() and ctx.EXISTS():
+            obj.existence_check = "if exists"
+        return obj
+
+
+class Constraint(AstNode):
+    _rules = [("out_of_line_constraint", "_from_constraint")]
+    _fields_spec = [
+        "constraint_name->name",
+        "type",
+        "column_name->columns",
+        "reference",
+    ]
+
+    @classmethod
+    def _from_constraint(cls, visitor, ctx):
+        obj = cls._from_fields(visitor, ctx)
+
+        foreign_key_ctx = ctx.foreign_key_clause()
+        if ctx.UNIQUE():
+            obj.type = "unique"
+        elif ctx.PRIMARY() and ctx.KEY():
+            obj.type = "primary_key"  # todo: format?
+        elif foreign_key_ctx and foreign_key_ctx.FOREIGN() and foreign_key_ctx.KEY():
+            obj.type = "foreign_key"
+            columns_ctx = foreign_key_ctx.paren_column_list().column_list()
+            obj.columns = visitor.visitChildren(
+                columns_ctx, predicate=is_terminal, simplify=False
+            )
+            reference_ctx = foreign_key_ctx.references_clause()
+            obj.reference = visitor.visit(reference_ctx)
+        elif ctx.CHECK():
+            obj.type = "check"
+        return obj
+
+
+class InsertStmt(AstNode):
+    _rules = [("insert_statement", "_from_single_table")]
+    _fields_spec = ["table", "columns", "values", "query"]
+
+    @classmethod
+    def _from_single_table(cls, visitor, ctx):
+        obj = cls._from_fields(visitor, ctx)
+        ctx = ctx.single_table_insert()
+
+        insert_into_ctx = ctx.insert_into_clause()
+        obj.table = visitor.visit(insert_into_ctx.general_table_ref())
+
+        columns_ctx = insert_into_ctx.column_name_list()
+        if columns_ctx:
+            obj.columns = visitor.visit(columns_ctx)
+
+        values_ctx = ctx.values_clause()
+        if values_ctx:
+            values_list_ctx = values_ctx.expression_list()
+            obj.values = visitor.visitChildren(
+                values_list_ctx, predicate=is_terminal, simplify=False
+            ).arr
+
+        query_ctx = ctx.select_statement()
+        if query_ctx:
+            obj.query = visitor.visit(query_ctx)
+
+        return obj
+
+
+class UpdateStmt(AstNode):
+    _rules = [("update_statement", "_from_update")]
+    _fields_spec = [
+        "general_table_ref->table",
+        "where_clause",
+        "from_clause",
+        "updates",
+    ]
+
+    @classmethod
+    def _from_update(cls, visitor, ctx):
+        obj = cls._from_fields(visitor, ctx)
+
+        update_set_ctx = ctx.update_set_clause()
+        if update_set_ctx.column_based_update_set_clause():
+            obj.updates = visitor.visitChildren(
+                update_set_ctx, predicate=is_terminal, simplify=False
+            )
+
+        return obj
+
+
+class Update(AstNode):
+    _rules = ["column_based_update_set_clause"]
+    _fields_spec = ["column", "expression"]
+
+
+class DeleteStmt(AstNode):
+    _rules = ["delete_statement"]
+    _fields_spec = ["general_table_ref->table", "where_clause"]
+
+
 # class FunctionArgument
 
 
@@ -320,7 +525,7 @@ class CaseWhen(AstNode):
 
 
 class AstVisitor(grammar.Visitor):
-    def visitChildren(self, node, predicate=None):
+    def visitChildren(self, node, predicate=None, simplify=True):
         result = self.defaultResult()
         n = node.getChildCount()
         for i in range(n):
@@ -334,21 +539,26 @@ class AstVisitor(grammar.Visitor):
             childResult = c.accept(self)
             result = self.aggregateResult(result, childResult)
 
-        return self.result_to_ast(node, result)
+        return self.result_to_ast(node, result, simplify=simplify)
 
     @staticmethod
-    def result_to_ast(node, result):
-        if len(result) == 1:
+    def result_to_ast(node, result, simplify=True):
+        if simplify and len(result) == 1:
             return result[0]
         elif len(result) == 0:
             return None
-        elif all(isinstance(res, str) for res in result):
+        elif simplify and all(isinstance(res, str) for res in result):
             return " ".join(result)
         elif all(
             isinstance(res, AstNode) and not isinstance(res, Unshaped) for res in result
         ):
             return result
         else:
+            if all(res is None for res in result):
+                # return unparsed text
+                result = node.start.getInputStream().getText(
+                    node.start.start, node.stop.stop
+                )
             return Unshaped(node, result)
 
     def defaultResult(self):
@@ -365,6 +575,9 @@ class AstVisitor(grammar.Visitor):
         if not (text[0] in quotes and text[-1] in quotes):
             text = text.lower()
         return text
+
+    def visitErrorNode(self, node):
+        return None
 
     def visitSubqueryParen(self, ctx):
         return self.visit(ctx.subquery())
@@ -384,6 +597,10 @@ class AstVisitor(grammar.Visitor):
 
     # def visitIs_part(self, ctx):
     #    return ctx
+
+    def visitConstraint_clauses(self, ctx):
+        if ctx.ADD():
+            return AddConstraints._from_fields(self, ctx)
 
     # simple dropping of tokens -----------------------------------------------
     # Note can't filter out TerminalNodeImpl from some currently as in something like
@@ -424,6 +641,13 @@ class AstVisitor(grammar.Visitor):
         "dml_table_expression_clause",
         "function_argument",
         "argument_list",
+        "paren_column_list",
+        "column_list",
+        "relational_table",
+        # "relational_properties",
+        "column_name_list",
+        "expression_list",
+        "update_set_clause",
     ]
 
 
@@ -437,13 +661,20 @@ for item in list(globals().values()):
         if getattr(item, "_rules", None) is not None:
             item._bind_to_visitor(AstVisitor)
 
+
+def is_terminal(node):
+    """Predicate to detect terminal nodes
+
+    Consider adding a node to the visitor _remove_terminal list.
+    """
+    return not isinstance(node, Tree.TerminalNode)
+
+
 # Override node visiting methods to add terminal child skipping in AstVisitor
 for rule in AstVisitor._remove_terminal:
     # f = partial(AstVisitor.visitChildren, predicate = lambda n: not isinstance(n, Tree.TerminalNode))
     def skip_terminal_child_nodes(self, ctx):
-        return self.visitChildren(
-            ctx, predicate=lambda n: not isinstance(n, Tree.TerminalNode)
-        )
+        return self.visitChildren(ctx, predicate=is_terminal)
 
     bind_to_visitor(AstVisitor, rule, skip_terminal_child_nodes)
 
@@ -455,6 +686,32 @@ speaker = Speaker(**speaker_cfg)
 
 if __name__ == "__main__":
     query = """
-SELECT id FROM artists WHERE id > 100
+-- Convert the values in firstname to a max. of 16 characters
+ALTER TABLE professors
+ALTER COLUMN firstname
+TYPE varchar(16)
+USING SUBSTRING(firstname FROM 1 FOR 16);
+
+
+
+INSERT INTO transactions (transaction_date, amount, fee)
+VALUES ('2018-09-24', 5454, '30');
+-- Rename the university_shortname column
+ALTER TABLE professors
+RENAME COLUMN university_shortname TO university_id;
+
+-- Add a foreign key on professors referencing universities
+ALTER TABLE professors
+ADD CONSTRAINT professors_fkey FOREIGN KEY (university_id) REFERENCES universities (id);
+ALTER TABLE affiliations
+ADD COLUMN professor_id integer REFERENCES professors (id);
+ALTER TABLE professors
+ADD CONSTRAINT professors_fkey FOREIGN KEY (university_id) REFERENCES universities (id);
+CREATE TEMP TABLE indicators AS
+  SELECT id,
+         CAST (description LIKE '%@%' AS INTEGER) AS email,
+         CAST (description LIKE '%___-___-____%' AS INTEGER) AS phone
+    FROM evanston311;
+
     """
     parse(query)
